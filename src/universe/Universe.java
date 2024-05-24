f97ca356ddee3f670cfa776842636230a8c59f06
Nascent <nasko6670@abv.bg>
2024-05-24 19:31:59 +0300

    Create Universe model

---
+++ src/universe/Universe.java
@@ -0,0 +1,63 @@
+package universe;
+
+import jedi.Jedi;
+import planet.Planet;
+
+import java.util.HashSet;
+
+public class Universe {
+    private String fileName;
+    private HashSet<Planet> planets = new HashSet<>();
+    private HashSet<Jedi> jediPopulation = new HashSet<>();
+    private static Universe universeInstance = null;
+    private boolean isFileOpened = false;
+
+    private Universe() {}
+
+    public static Universe getInstance() {
+        if (universeInstance == null) {
+            universeInstance = new Universe();
+        }
+        return universeInstance;
+    }
+
+    public static Universe getUniverseInstance() {
+        return universeInstance;
+    }
+
+    public void readFromXml(String fileName) {
+        this.fileName = fileName;
+        System.out.println(fileName);
+        XmlRead xmlRead = new XmlRead();
+        xmlRead.readXmlFile(fileName);
+        isFileOpened = true;
+    }
+
+    public HashSet<Jedi> getJediPopulation() {
+        return jediPopulation;
+    }
+
+    public void setJediPopulation(HashSet<Jedi> jediPopulation) {
+        this.jediPopulation = jediPopulation;
+    }
+
+    public HashSet<Planet> getPlanets() {
+        return planets;
+    }
+
+    public void setPlanets(HashSet<Planet> planets) {
+        this.planets = planets;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileOpened(boolean fileOpened) {
+        isFileOpened = fileOpened;
+    }
+
+    public boolean isFileOpened() {
+        return isFileOpened;
+    }
+}
