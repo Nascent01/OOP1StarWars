eb88d38bf81994900c4b2f7e46442fbea146ce18
Nascent <nasko6670@abv.bg>
2024-05-25 10:19:29 +0300

    Add more case handling in CommandFactory class

--- src/cli/CommandFactory.java
+++ src/cli/CommandFactory.java
@@ -1,12 +1,22 @@
 package cli;
 
-import command.Open;
+import command.*;
 
 public abstract class CommandFactory {
     public static Command receiveCommand(String command) {
         switch (command.toLowerCase()) {
             case "open":
                 return new Open();
+            case "save":
+                return new Save();
+            case "saveas":
+                return new SaveAs();
+            case "help":
+                return new Help();
+            case "close":
+                return new Close();
+            case "exit":
+                return new Exit();
         }
 
         return null;
