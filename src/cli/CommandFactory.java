5e53cab603a0c3b581bdefcef82dc253f7a34256
Nascent <nasko6670@abv.bg>
2024-05-25 22:19:36 +0300

    Change order of commands in alphabetical order in CommandFactory class

--- src/cli/CommandFactory.java
+++ src/cli/CommandFactory.java
@@ -7,36 +7,36 @@ public abstract class CommandFactory {
         switch (command.toLowerCase()) {
             case "add_planet":
                 return new AddPlanet();
+            case "close":
+                return new Close();
             case "create_jedi":
                 return new CreateJedi();
             case "demote_jedi":
                 return new DemoteJedi();
+            case "exit":
+                return new Exit();
+            case "get_most_used_saber_color":
+                return new GetMostUsedSaberColor();
             case "get_strongest_jedi":
                 return new GetStrongestJedi();
             case "get_youngest_jedi":
                 return new GetYoungestJedi();
+            case "help":
+                return new Help();
             case "open":
                 return new Open();
+            case "planet_name":
+                return new PlanetName();
+            case "print":
+                return new Print();
+            case "promote_jedi":
+                return new PromoteJedi();
+            case "remove_jedi":
+                return new RemoveJedi();
             case "save":
                 return new Save();
             case "saveas":
                 return new SaveAs();
-            case "help":
-                return new Help();
-            case "planet_name":
-                return new PlanetName();
-            case "close":
-                return new Close();
-            case "exit":
-                return new Exit();
-            case "promote_jedi":
-                return new PromoteJedi();
-            case "get_most_used_saber_color":
-                return new GetMostUsedSaberColor();
-            case "print":
-                return new Print();
-            case "remove_jedi":
-                return new RemoveJedi();
             default:
                 System.out.println("Bad command. Try again or use help.");
                 return null;
