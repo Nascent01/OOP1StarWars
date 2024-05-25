4bf050b856f698c3474661d41aecdbb74aa7e04e
Nascent <nasko6670@abv.bg>
2024-05-25 10:46:55 +0300

    Fix save command error in logic

--- src/command/Save.java
+++ src/command/Save.java
@@ -14,14 +14,14 @@ public class Save implements Command {
     @Override
     public void execute(String[] args) throws IOException, ParserConfigurationException, TransformerException {
         if (!Universe.getInstance().isFileOpened()) {
-           if (args.length != 1) {
-               Universe universe = Universe.getInstance();
-               XmlWrite xmlWrite = new XmlWrite();
-               xmlWrite.writeXmlFile(universe, universe.getFileName());
-               System.out.println("Saved " + universe.getFileName() + " to " + universe.getFileName());
-           }
-        } else {
             System.out.println("You must open a file first");
+        } else if (args.length != 1) {
+            System.out.println("Usage: save all changes in currently opened file");
+        } else {
+            Universe universe = Universe.getInstance();
+            XmlWrite xmlWrite = new XmlWrite();
+            xmlWrite.writeXmlFile(universe, universe.getFileName());
+            System.out.println("Saved " + universe.getFileName() + " to " + universe.getFileName());
         }
     }
 }
